b547512836e5bdf6e653ba8e27c83afe9bd5dfd2
Vineeth-07 <vineethdvs7@gmail.com>
2023-04-29 00:37:52 +0530

    Level 3

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TaskList from "./TaskList";
+import TaskForm from './TaskFrom';
+import TaskApp from './TaskApp';
+function App() {
+  return (
+    <div className="App">
+      <TaskApp />
+    </div>
+  );
+}
+export default App;
