277ceb91e099ce95f26f4c2b7c641825709d1d1c
Vineeth-07 <vineethdvs7@gmail.com>
2023-05-08 18:25:10 +0530

    added TailwindCSS

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -43,47 +43,68 @@ const TaskForm = (props: TaskFormProps) => {
   };
   return (
     <form onSubmit={addTask}>
-      <div>
-        <label htmlFor="title" className="required">
-          Title :{" "}
-        </label>
-        <input
-          type="text"
-          name="title"
-          id="todoTitle"
-          placeholder="Enter title"
-          value={formState.title}
-          onChange={titleChanged}
-          required
-        />
-        <br />
-        <label htmlFor="todoDescription">Description : </label>
-        <input
-          type="text"
-          name="todoDescription"
-          id="todoDescription"
-          placeholder="Enter description"
-          value={formState.description}
-          onChange={descriptionChanged}
-        />
-        <br />
-        <label htmlFor="tododueDate" className="required">
-          Due date :{" "}
-        </label>
-        <input
-          type="date"
-          name="tododueDate"
-          id="tododueDate"
-          value={formState.dueDate}
-          onChange={dueDateChanged}
-          required
-        />
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            id="todoTitle"
+            name="todoTitle"
+            type="text"
+            value={formState.title}
+            onChange={titleChanged}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+          />
+          <label
+            htmlFor="todoTitle"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Todo Title
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            id="todoDescription"
+            name="todoDescription"
+            type="text"
+            value={formState.description}
+            onChange={descriptionChanged}
+            placeholder=" "
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+          <label
+            htmlFor="todoDescription"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Description
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            id="todoDueDate"
+            name="todoDueDate"
+            type="date"
+            value={formState.dueDate}
+            onChange={dueDateChanged}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="todoDueDate"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Due Date
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Add item
+          </button>
+        </div>
       </div>
-      <button type="submit" id="addTaskButton">
-        Add item
-      </button>
-      <br />
-      <br />
     </form>
   );
 };
