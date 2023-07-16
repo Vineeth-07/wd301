82166ba6c81263b6a7185df10acc1380d767513f
Vineeth-07 <vineethdvs7@gmail.com>
2023-07-16 10:51:07 +0530

    Resolved minor bug for dark theme

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,13 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes"
 import { ThemeContext } from "./context/theme";
 const App = () => {
   const currentTheme = useContext(ThemeContext)
+  useEffect(() => {
+    if(currentTheme.theme === 'dark'){
+      document.documentElement.classList.add('dark')
+    }else{
+      document.documentElement.classList.remove('dark')
+    }
+  }, [currentTheme.theme])
   return (
     <div>
-      {currentTheme.theme}
       <RouterProvider router={router} />
     </div>
   );
