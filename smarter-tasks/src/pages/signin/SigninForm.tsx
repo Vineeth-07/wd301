29ab09c5361d1b2d0e1e5fb03b5dc92816592acb
Vineeth-07 <vineethdvs7@gmail.com>
2023-07-21 19:17:58 +0530

    Updated forms

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -1,48 +1,87 @@
-import React, { useState } from 'react';
-// Dialogue 1: First we will import the API_ENDPOINT constant from the `config` folder
-import { API_ENDPOINT } from '../../config/constants';
-import  {useNavigate}  from "react-router-dom";
+import React from "react";
+import { API_ENDPOINT } from "../../config/constants";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const SigninForm: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  // Dialogue 2: Then we will define the handle submit function
-  const navigate=useNavigate();
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { email, password } = data;
+
     try {
       const response = await fetch(`${API_ENDPOINT}/users/sign_in`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
       });
-      const data=await response.json();
+
       if (!response.ok) {
-        throw new Error('Sign-in failed');
+        throw new Error("Sign-in failed");
       }
 
-      console.log('Sign-in successful');
-      localStorage.setItem('authToken', data.token);
-      localStorage.setItem('userData', JSON.stringify(data.user));
-      // Dialogue: After successful signin we have to redirect the user to the secured page. We will do that later.
-      navigate("/account")
+      console.log("Sign-in successful");
+      const data = await response.json();
 
+      localStorage.setItem("authToken", data.token);
+      localStorage.setItem("userData", JSON.stringify(data.user));
+
+      navigate("/account");
     } catch (error) {
-      console.error('Sign-in failed:', error);
+      console.error("Sign-in failed:", error);
     }
   };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-        <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <input
+          type="email"
+          id="email"
+          {...register("email", { required: true })}
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        {errors.email && (
+          <span className="text-red-500">This field is required</span>
+        )}
       </div>
       <div>
-        <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <label className="block text-gray-700 font-semibold mb-2">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          {...register("password", { required: true })}
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        {errors.password && (
+          <span className="text-red-500">This field is required</span>
+        )}
       </div>
-      <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
+      <button
+        type="submit"
+        className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4"
+      >
+        Sign In
+      </button>
+      <p style={{ fontStyle: "italic" }}>
+        Don't have accont?{" "}
+        <a href="/signup" style={{ color: "blue" }}>
+          Signup here
+        </a>
+      </p>
     </form>
   );
 };
