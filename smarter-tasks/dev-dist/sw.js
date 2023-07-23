f0ec8979b28a7af7c0727c63b75e91e68e352840
Vineeth-07 <vineethdvs7@gmail.com>
2023-07-23 12:00:02 +0530

    Added PWA

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-715e063a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rs6ubv2p7ho"
+    "revision": "0.1n8a9bagrno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
